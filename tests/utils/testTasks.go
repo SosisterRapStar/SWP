390b1a25bd6d15709d06424df20018d98cd4d26c
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-06-08 14:37:17 +0300

    added tests and goleak

--- tests/utils/testTasks.go
+++ tests/utils/testTasks.go
@@ -23,6 +23,28 @@ func Ticker() error {
 	}
 }
 
+func TickerWithVarStopTime(stopDur int) func() error {
+
+	return func() error {
+		taskId := 1
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		timeout := time.After(time.Duration(stopDur) * time.Second)
+
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Printf("Task %v: tick \n", taskId)
+			case <-timeout:
+				fmt.Printf("Stopped: %v\n", taskId)
+				return nil
+			}
+		}
+	}
+
+}
+
 func Closure(start int) func() error {
 
 	return func() error {
